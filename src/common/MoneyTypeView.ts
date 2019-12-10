53f7c08efcab4895faf44311f8b455fc72cab60a
shenyusheng <shenyusheng@hoodinn.com>
2019-12-10 16:32:11 +0800

    homeDlg

---
+++ src/common/MoneyTypeView.ts
@@ -0,0 +1,16 @@
+namespace app.common {
+    export class MoneyTypeView extends MoneyTypeViewUI implements IMoneyTypeViewUI {
+        
+        protected _modelEvents: any[] = [];
+
+        onCreate() {
+            super.onCreate();
+        }
+
+        
+        
+        onBtnReceiveClick(e: Laya.Event): void { }
+        
+    }
+    
+}
---
+++ src/common/PowerView.ts
@@ -0,0 +1,13 @@
+namespace app.common {
+    export class PowerView extends PowerViewUI  {
+        
+        protected _modelEvents: any[] = [];
+
+        onCreate() {
+            super.onCreate();
+        }
+
+        
+    }
+    
+}
