1cfa41b787fe5bb3721122bec7fc4bb0e88512b9
swp <songwenping@shzhanmeng.com>
2019-12-11 10:28:27 +0800

    ui修改

--- src/utils/resname.ts
+++ src/utils/resname.ts
@@ -1,4 +1,9 @@
 namespace r {
     export let JSON = ".json";
 
+    export function getIdiomGameCellBg(state: number) {
+        if (state == 3) state = 2;
+        return `fight/youxinei_tu${state}.png`
+    }
+
 }
